61225a12568efb556b886660c5ba4f5a37b9d9a8
quachquynh <quachquynh93@gmail.com>
2021-07-21 10:32:12 +0700

    Code chức năng Wordpress

---
+++ chuc-nang.php
@@ -0,0 +1,9 @@
+function new_excerpt_length($length) {
+return 30;
+}
+add_filter('excerpt_length', 'new_excerpt_length');
+
+function new_excerpt_more($more) {
+return '...<a href="'. get_permalink() . '">' . ' Xem thêm' . '</a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
