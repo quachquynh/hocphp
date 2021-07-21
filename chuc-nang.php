7a690ca062feffe115e896b136954c7eafe73a28
quachquynh <quachquynh93@gmail.com>
2021-07-21 10:52:03 +0700

    Update chuc-nang.php

--- chuc-nang.php
+++ chuc-nang.php
@@ -67,3 +67,47 @@ textarea {
     cursor: pointer;
     border-radius: 5px;
 }
+
+// Đăng ký sidebar
+function mytheme_widgets_init() {
+register_sidebar( array(
+'name' => esc_html__( 'Sidebar', 'mytheme' ),
+'id' => 'sidebar-1',
+'description' => esc_html__( 'Add widgets here.', 'mytheme' ),
+'before_widget' => '<section id="%1$s" class="widget %2$s">',
+'after_widget' => '</section>',
+'before_title' => '<h2 class="widget-title">',
+'after_title' => '</h2>',
+) );
+
+register_sidebar( array(
+'name' => esc_html__( 'Footer 1', 'mytheme' ),
+'id' => 'footer-1',
+'description' => esc_html__( 'Add widgets here.', 'mytheme' ),
+'before_widget' => '<div id="%1$s" class="widget %2$s">',
+'after_widget' => '</div>',
+'before_title' => '<h4 class="widget-title">',
+'after_title' => '</h4>',
+) );
+
+register_sidebar( array(
+'name' => esc_html__( 'Footer 2', 'mytheme' ),
+'id' => 'footer-2',
+'description' => esc_html__( 'Add widgets here.', 'mytheme' ),
+'before_widget' => '<div id="%1$s" class="widget %2$s">',
+'after_widget' => '</div>',
+'before_title' => '<h4 class="widget-title">',
+'after_title' => '</h4>',
+) );
+
+register_sidebar( array(
+'name' => esc_html__( 'Footer 3', 'mytheme' ),
+'id' => 'footer-3',
+'description' => esc_html__( 'Add widgets here.', 'mytheme' ),
+'before_widget' => '<div id="%1$s" class="widget %2$s">',
+'after_widget' => '</div>',
+'before_title' => '<h4 class="widget-title">',
+'after_title' => '</h4>',
+) );
+}
+add_action( 'widgets_init', 'mytheme_widgets_init' );
